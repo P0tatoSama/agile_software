150233923e3363d16eb70dd7e14cc3d10e40bcb6
P0tatoSama <51224652+P0tatoSama@users.noreply.github.com>
2019-11-29 08:48:07 +0800

    Add files via upload

---
+++ agile run 2(latest)/WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Process.Start("iexplore", "http://www.google.com/search?hl=en&q=" + TextBox1.Text + "&aq=f&oq=")
+        Form2.ListBox1.Items.Add(TextBox1.Text)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Form2.Show()
+    End Sub
+End Class
