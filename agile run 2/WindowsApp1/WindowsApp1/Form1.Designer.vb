ae5ab1a7202cfa02a5eaf7d09bdb8d21b669ab27
P0tatoSama <51224652+P0tatoSama@users.noreply.github.com>
2019-11-29 08:44:43 +0800

    Add files via upload

--- agile run 2/WindowsApp1/WindowsApp1/Form1.Designer.vb
+++ agile run 2/WindowsApp1/WindowsApp1/Form1.Designer.vb
@@ -23,8 +23,9 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.ListBox1 = New System.Windows.Forms.ListBox()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
         Me.SuspendLayout()
         '
         'Button1
@@ -36,14 +37,6 @@ Partial Class Form1
         Me.Button1.Text = "Search"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'ListBox1
-        '
-        Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.Location = New System.Drawing.Point(142, 159)
-        Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(213, 160)
-        Me.ListBox1.TabIndex = 1
-        '
         'TextBox1
         '
         Me.TextBox1.Location = New System.Drawing.Point(142, 49)
@@ -51,13 +44,31 @@ Partial Class Form1
         Me.TextBox1.Size = New System.Drawing.Size(132, 20)
         Me.TextBox1.TabIndex = 2
         '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 296)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(89, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Search History"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(95, 87)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(287, 173)
+        Me.ListBox1.TabIndex = 4
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(479, 331)
-        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -67,6 +78,7 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents Button1 As Button
-    Friend WithEvents ListBox1 As ListBox
     Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ListBox1 As ListBox
 End Class
--- agile run 2/WindowsApp1/WindowsApp1/Form1.vb
+++ agile run 2/WindowsApp1/WindowsApp1/Form1.vb
@@ -2,8 +2,10 @@
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Process.Start("iexplore", "http://www.google.com/search?hl=en&q=" + TextBox1.Text + "&aq=f&oq=")
-        ListBox1.Items.Add(TextBox1.Text)
+        Form2.ListBox1.Items.Add(TextBox1.Text)
     End Sub
 
-
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Form2.Show()
+    End Sub
 End Class
